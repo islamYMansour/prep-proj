141810cc752adb4c58217804b5148015292f6d35
islamYMansour <islamy95@hotmail.com>
2019-09-19 10:21:20 +0300

    time and date

--- scripts.js
+++ scripts.js
@@ -2,11 +2,15 @@
 var myList = createToDoList();
 var listElement = $("#list")
 var listItems = $('#list li');
+
 function createToDoList() {
 	var result = {}
 
 	result.list =[]
 	result.add = addToList
+	result.date = getDate
+	result.reset = reset
+	result.time= getTime
 	
 	return result
 }
@@ -43,3 +47,30 @@ $("#list").click(function(ev) {
 	
 })
 
+function getDate() {
+	var date = new Date ()
+	var result = ((date.getMonth().toString().length > 1) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate().toString().length > 1) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear()
+	return result
+}
+
+function reset () {
+	$('#list').empty();
+}
+
+function getTime () {
+	var date = new Date ()
+	var result = ((date.getHours().toString().length > 1) ? (date.getHours() ) : ('0' + (date.getHours() ))) + ':' 
+	+ ((date.getMinutes().toString().length > 1) ? date.getMinutes() : ('0' + date.getMinutes())) + ':' + ((date.getSeconds().toString().length > 1) ? date.getSeconds() : ('0' + date.getSeconds()))
+	return result
+}
+	$('#dateID').text(myList.date())
+
+setInterval(function() { 
+	console.log(myList.time())
+	$('#timeID').text(myList.time())
+
+}, 1000);
+
+$('#reset').click(function() {
+	myList.reset()
+})
